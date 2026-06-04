dd525eb05729a5a7bf4467aee1d2f6595dba09ee
Cahayani Dinda Permatasari <cahayanidindapermatasari@gmail.com>
2026-06-04 12:52:23 +0700

    update logic excercise

---
+++ LogicExcercise/Program.cs
@@ -0,0 +1,15 @@
+﻿void fooBar (int x)
+{
+    for(int i = 1; i <= x; i++)
+    {
+        string result = "";
+        if (i % 3 == 0) result += "foo";
+        if (i % 5 == 0) result += "bar";
+
+        Console.Write(result == "" ? i.ToString() : result);
+
+        if (i < x) Console.Write(", ");
+    }
+}
+
+fooBar(15);
