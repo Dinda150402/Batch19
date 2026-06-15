2baa5854eb79d3408adca951edd097a4239e6b6f
Cahayani Dinda Permatasari <cahayanidindapermatasari@gmail.com>
2026-06-15 11:52:00 +0700

    LogicExcercise 3, dimana jika x% 4 = 0 akan print "bazz"

--- LogicExcercise/Program.cs
+++ LogicExcercise/Program.cs
@@ -4,6 +4,8 @@
     {
         string result = "";
         if (i % 3 == 0) result += "foo";
+        //LogicExcercise 3, dimana jika x habis dibagi 4 akan print "baz"
+        if (i % 4 == 0) result += "baz";
         if (i % 5 == 0) result += "bar";
         //LogicExcercise 2, dimana jika x habis dibagi 7 akan print "jazz"
         if (i % 7 == 0) result += "jazz";
