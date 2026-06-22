86a7846fb5feb56d50f354f70e8764ed45e7c909
Cahayani Dinda Permatasari <cahayanidindapermatasari@gmail.com>
2026-06-22 10:26:22 +0700

    Final LogicExcercise

--- LogicExcercise/Program.cs
+++ LogicExcercise/Program.cs
@@ -1,4 +1,11 @@
-﻿void fooBar (int x)
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.VisualBasic;
+
+/*void fooBar (int x)
 {
     for(int i = 1; i <= x; i++)
     {
@@ -18,4 +25,41 @@
     }
 }
 
-fooBar(21);
+fooBar(21);*/
+
+// Final LogicExcercise
+var generator = new FooBarGenerator();
+
+generator.AddRule(3, "foo");
+generator.AddRule(4, "baz");
+generator.AddRule(5, "bar");
+generator.AddRule(7, "jazz");
+generator.AddRule(9, "huzz");
+
+generator.Print(21);
+
+public class FooBarGenerator
+{
+    private readonly List<(int divisor, string text)> rules = new();
+
+    public void AddRule(int divisor, string text)
+    {
+        rules.Add((divisor, text));
+    }
+
+    public void Print (int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            var sb = new StringBuilder();
+            foreach(var rule in rules)
+                if(i % rule.divisor == 0) sb.Append(rule.text);
+                    
+            Console.Write(sb.Length == 0 ? i.ToString() : sb.ToString());
+            
+            if (i < n) Console.Write(", ");
+            
+        }
+    }
+
+}
