d67948c8694089b0338d164255cfb54bc1b72c9b
Cahayani Dinda Permatasari <cahayanidindapermatasari@gmail.com>
2026-07-08 09:15:09 +0700

    refactor: change ordering of departments to use Id instead of Name in repository methods

--- CRUD-EFCore WebAPI/Repositories/DepartmentRepository.cs
+++ CRUD-EFCore WebAPI/Repositories/DepartmentRepository.cs
@@ -9,12 +9,18 @@ namespace CRUDEFCore.Repositories
         public DepartmentRepository(AppDbContext db) : base(db) { }
 
         public async Task<List<Department>> GetAllWithEmployeesAsync() =>
-            await _db.Departments.Include(d => d.Employees).OrderBy(d => d.Name).ToListAsync();
+            await _db.Departments
+                .Include(d => d.Employees)
+                .OrderBy(d => d.Id)
+                .ToListAsync();
 
         public async Task<Department?> GetByIdWithEmployeesAsync(int id) =>
-            await _db.Departments.Include(d => d.Employees).FirstOrDefaultAsync(d => d.Id == id);
+            await _db.Departments
+                .Include(d => d.Employees)
+                .FirstOrDefaultAsync(d => d.Id == id);
 
         public async Task<bool> ExistsByNameAsync(string name) =>
-            await _db.Departments.AnyAsync(d => d.Name.ToLower() == name.ToLower());
+            await _db.Departments
+                .AnyAsync(d => d.Name.ToLower() == name.ToLower());
     }
 }
